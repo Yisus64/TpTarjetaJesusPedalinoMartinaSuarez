a03d1298ddc270b0f1c67a7cf7282e885e94792b
Yisus64 <110793348+Yisus64@users.noreply.github.com>
2024-11-05 07:34:33 -0300

    Interurbano

--- TpTarjetaJesusPedalino/Colectivo.cs
+++ TpTarjetaJesusPedalino/Colectivo.cs
@@ -4,20 +4,54 @@ using System.Collections.Generic;
 namespace TpSube{
     public class Colectivo
     {
-        public static int valorPasaje = 940;
-        public static int valorPasajeMedio = valorPasaje/2;
-        public static int valorPasajeCompleto = 0; // se refiere al valor de una franquicia completa
+        public static float valorPasaje;
+        public static float valorPasajeMedio;
+        public static int valorPasajeCompleto; // se refiere al valor de una franquicia completa
         public string linea;
         public float saldo_pendiente = 0;
+        public float tarifaNormal; // se refiere a la tarifa que paga una tarjeta normal dependiendo de cuantos viajes tenga en el mes
 
         public Colectivo(string lineaDelBondi)
         {
             linea = lineaDelBondi;
+            valorPasaje = 1200;
+            valorPasajeMedio = valorPasaje / 2;
+            valorPasajeCompleto = 0;
         }
         public Boleto pagarCon(Tarjeta tarjeta, Tiempo tiempo)
         {
             if (tarjeta is FranquiciaCompleta)
             {
+                if (tiempo.Now().DayOfWeek == DayOfWeek.Saturday || tiempo.Now().DayOfWeek == DayOfWeek.Sunday || tiempo.Now().Hour < 6 || tiempo.Now().Hour > 22)
+                {
+                    tarjeta.saldo -= valorPasaje;
+                    Boleto boleto = new Boleto();
+                    boleto.costo = valorPasaje;
+                    boleto.fechaUltimoViaje = tiempo.Now();
+                    boleto.lineaDeColectivo = linea;
+                    boleto.tipoDeTarjeta = "Franquicia Completa";
+                    boleto.saldoTarjeta = tarjeta.saldo;
+                    boleto.idTarjeta = tarjeta.ID;
+                    tarjeta.boletos.Add(boleto);
+                    tarjeta.cantViajesHoy++;
+                    tarjeta.cantViajesMes++;
+                    if (tarjeta.pendiente > 0)
+                    {
+                        if (tarjeta.pendiente >= boleto.costo)
+                        {
+                            tarjeta.saldo += boleto.costo;
+                            tarjeta.pendiente -= boleto.costo;
+                            tarjeta.checkPendiente();
+                        }
+                        else
+                        {
+                            tarjeta.saldo += tarjeta.pendiente;
+                            tarjeta.pendiente = 0;
+                        }
+
+                    }
+                    return boleto;
+                }
                 if (tarjeta.boletos.Count != 0)
                 {
                     if (tarjeta.ultimoBoleto().fechaUltimoViaje.DayOfYear != tiempo.Now().DayOfYear)
@@ -35,6 +69,7 @@ namespace TpSube{
                         boleto.idTarjeta = tarjeta.ID;
                         tarjeta.boletos.Add(boleto);
                         tarjeta.cantViajesHoy++;
+                        tarjeta.cantViajesMes++;
                         return boleto;
                     } else
                     {
@@ -48,6 +83,7 @@ namespace TpSube{
                         boleto.idTarjeta = tarjeta.ID;
                         tarjeta.boletos.Add(boleto);
                         tarjeta.cantViajesHoy++;
+                        tarjeta.cantViajesMes++;
                         if (tarjeta.pendiente > 0)
                         {
                             if (tarjeta.pendiente >= boleto.costo)
@@ -76,11 +112,42 @@ namespace TpSube{
                     boleto.idTarjeta = tarjeta.ID;
                     tarjeta.boletos.Add(boleto);
                     tarjeta.cantViajesHoy++;
+                    tarjeta.cantViajesMes++;
                     return boleto;
                 }
                     
             } else if (tarjeta is FranquiciaMedia)
             {
+                if (tiempo.Now().DayOfWeek == DayOfWeek.Saturday || tiempo.Now().DayOfWeek == DayOfWeek.Sunday || tiempo.Now().Hour < 6 || tiempo.Now().Hour > 22)
+                {
+                    tarjeta.saldo -= valorPasaje;
+                    Boleto boleto = new Boleto();
+                    boleto.costo = valorPasaje;
+                    boleto.fechaUltimoViaje = tiempo.Now();
+                    boleto.lineaDeColectivo = linea;
+                    boleto.tipoDeTarjeta = "Franquicia Media";
+                    boleto.saldoTarjeta = tarjeta.saldo;
+                    boleto.idTarjeta = tarjeta.ID;
+                    tarjeta.boletos.Add(boleto);
+                    tarjeta.cantViajesHoy++;
+                    tarjeta.cantViajesMes++;
+                    if (tarjeta.pendiente > 0)
+                    {
+                        if (tarjeta.pendiente >= boleto.costo)
+                        {
+                            tarjeta.saldo += boleto.costo;
+                            tarjeta.pendiente -= boleto.costo;
+                            tarjeta.checkPendiente();
+                        }
+                        else
+                        {
+                            tarjeta.saldo += tarjeta.pendiente;
+                            tarjeta.pendiente = 0;
+                        }
+
+                    }
+                    return boleto;
+                }
                 if (tarjeta.boletos.Count == 0)
                 {
                     tarjeta.saldo -= valorPasajeMedio;
@@ -93,6 +160,7 @@ namespace TpSube{
                     boleto.idTarjeta = tarjeta.ID;
                     tarjeta.boletos.Add(boleto);
                     tarjeta.cantViajesHoy++;
+                    tarjeta.cantViajesMes++;
                     if (tarjeta.pendiente > 0)
                     {
                         if (tarjeta.pendiente >= boleto.costo)
@@ -140,6 +208,7 @@ namespace TpSube{
                                 boleto.idTarjeta = tarjeta.ID;
                                 tarjeta.boletos.Add(boleto);
                                 tarjeta.cantViajesHoy++;
+                                tarjeta.cantViajesMes++;
                                 if (tarjeta.pendiente > 0)
                                 {
                                     if (tarjeta.pendiente >= boleto.costo)
@@ -169,6 +238,7 @@ namespace TpSube{
                             boleto.idTarjeta = tarjeta.ID;
                             tarjeta.boletos.Add(boleto);
                             tarjeta.cantViajesHoy++;
+                            tarjeta.cantViajesMes++;
                             if (tarjeta.pendiente > 0)
                             {
                                 if (tarjeta.pendiente >= boleto.costo)
@@ -199,6 +269,7 @@ namespace TpSube{
                         boleto.idTarjeta = tarjeta.ID;
                         tarjeta.boletos.Add(boleto);
                         tarjeta.cantViajesHoy++;
+                        tarjeta.cantViajesMes++;
                         if (tarjeta.pendiente > 0)
                         {
                             if (tarjeta.pendiente >= boleto.costo)
@@ -218,12 +289,8 @@ namespace TpSube{
                     }
                   }
               } else //pasaje normal
-            {
-                if ((tarjeta.saldo + tarjeta.perdonDivino()) < valorPasaje)
                 {
-                    throw new Exception("Saldo insuficiente");
-                }
-                else
+                if (tarjeta.boletos.Count == 0)
                 {
                     tarjeta.saldo -= valorPasaje;
                     Boleto boleto = new Boleto();
@@ -235,6 +302,57 @@ namespace TpSube{
                     boleto.idTarjeta = tarjeta.ID;
                     tarjeta.boletos.Add(boleto);
                     tarjeta.cantViajesHoy++;
+                    tarjeta.cantViajesMes++;
+                    if (tarjeta.pendiente > 0)
+                    {
+                        if (tarjeta.pendiente >= boleto.costo)
+                        {
+                            tarjeta.saldo += boleto.costo;
+                            tarjeta.pendiente -= boleto.costo;
+                            tarjeta.checkPendiente();
+                        }
+                        else
+                        {
+                            tarjeta.saldo += tarjeta.pendiente;
+                            tarjeta.pendiente = 0;
+                        }
+                    }
+                    return boleto;
+                }
+                if(tarjeta.ultimoBoleto().fechaUltimoViaje.Month != tiempo.Now().Month)
+                {
+                    tarjeta.cantViajesMes = 0;
+                }
+
+                if (tarjeta.cantViajesMes < 29)
+                {
+                    tarifaNormal = valorPasaje;
+                } else if (tarjeta.cantViajesMes >= 29 && tarjeta.cantViajesMes < 79)
+                {
+                    tarifaNormal = (float)(valorPasaje * 0.8);
+                } else if (tarjeta.cantViajesMes >= 79)
+                {
+                    tarifaNormal = (float)(valorPasaje * 0.75);
+                }
+
+
+                if ((tarjeta.saldo + tarjeta.perdonDivino()) < tarifaNormal)
+                {
+                    throw new Exception("Saldo insuficiente");
+                }
+                else
+                {
+                    tarjeta.saldo -= tarifaNormal;
+                    Boleto boleto = new Boleto();
+                    boleto.costo = tarifaNormal;
+                    boleto.fechaUltimoViaje = tiempo.Now();
+                    boleto.lineaDeColectivo = linea;
+                    boleto.tipoDeTarjeta = "Sin Franquicia";
+                    boleto.saldoTarjeta = tarjeta.saldo;
+                    boleto.idTarjeta = tarjeta.ID;
+                    tarjeta.boletos.Add(boleto);
+                    tarjeta.cantViajesHoy++;
+                    tarjeta.cantViajesMes++;
                     if (tarjeta.pendiente > 0)
                     {
                         if (tarjeta.pendiente >= boleto.costo)
@@ -255,11 +373,11 @@ namespace TpSube{
             }   
         }
 
-        public int getValorPasaje()
+        public float getValorPasaje()
         {
             return valorPasaje;
         }
-        public int getValorPasajeMedio()
+        public float getValorPasajeMedio()
         {
             return valorPasajeMedio;
         }
